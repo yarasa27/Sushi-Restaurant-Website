911e02bbd9cbb889e092f8be27fd48f473dc0d9c
yarasa27 <elcan.abbasoff27@gmail.com>
2025-08-03 16:30:34 +0400

    show-menu close and open in JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
-/*=============== SHOW MENU ===============*/
+/* SHOW MENU */
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
 
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
