f9b76883cafb197df0da4dacc8c81abf2f8f4ed7
yarasa27 <elcan.abbasoff27@gmail.com>
2025-08-04 00:09:38 +0400

    scroll action link JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,10 +33,28 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
 
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId)
+
+        if(scrollY > sectionTop && scrollY <= sectionTop)
+
+    })
+}
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
