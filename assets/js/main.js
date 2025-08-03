ac558c073bf20ea0a727264d285622366905da29
yarasa27 <elcan.abbasoff27@gmail.com>
2025-08-03 16:47:05 +0400

    home html

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,14 @@ if(navClose){
     })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
